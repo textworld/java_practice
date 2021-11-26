5a9cd1abc5bfb5de3189c523caddbbe7d28a7a91
textworld <text.zwb@gmail.com>
2021-11-27 00:42:22 +0800

    feat: 增加了Cursor接口

---
+++ embbed_sql/src/main/java/cn/textworld/java/textdb/cursor/Cursor.java
@@ -0,0 +1,22 @@
+package cn.textworld.java.textdb.cursor;
+
+import cn.textworld.java.textdb.Table;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public interface Cursor {
+    String tableName();
+
+    boolean advance() throws NoSuchElementException;
+
+    Object column(String columnName);
+
+    Iterator<String> columns();
+
+    boolean isTraversing(Table t);
+
+    Object update(String columnName, Object newValue);
+
+    void delete();
+}
